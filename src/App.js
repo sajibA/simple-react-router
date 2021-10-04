18a64e235f7b9a38ee97d16b5e85810a00925bbf
sajibA <sajib06bd@gmail.com>
2021-10-04 10:01:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import About from './Components/About/About';
+import Friends from './Components/Friends/Friends';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './Components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This is not found page</h2>
+            <p>If you aske me kabjab.I will sent you Not Found!!!</p>
+        </div>
+    );
+};
+
+export default NotFound;
